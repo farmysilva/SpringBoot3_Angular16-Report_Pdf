e70322599d36325460d0d9c00db195d7f2af593d
Farmy Silva <farmysilva@yahoo.com.br>
2023-05-13 22:42:24 -0300

    Backend Funfando - No PDF

--- backend/src/main/java/com/farmy/backend/BackendApplication.java
+++ backend/src/main/java/com/farmy/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.farmy.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackageClasses = "com.farmy.backend")
+@SpringBootApplication(scanBasePackages = "com.farmy.backend")
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/farmy/backend/service/PetsService.java
+++ backend/src/main/java/com/farmy/backend/service/PetsService.java
@@ -9,18 +9,18 @@ import com.farmy.backend.model.Pets;
 @Service
 public interface PetsService {
     //create a new Pets
-    public Pets createPets();
+    public Pets createPets(Pets pets);
 
     //read the pets
     public List<Pets> readPets();
 
     //read the pets by id
-    public Pets readPetsById(int id);
+    public Pets readPetsById(Long id);
 
     //delete one pet
-    public void deletePet(int id);
+    public void deletePet(Long id);
 
     //update one pet
-    public Pets updatePet(Pets pets, int id);
+    public Pets updatePet(Pets pets, Long id);
     
 }
